9f028f0096f56bd3d4ba3c7af8d5810c65ea5b9b
fabio guelfi <fabioguelfiunix@gmail.com>
2018-09-21 22:48:39 -0300

    Mutation na API GraphQL com HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,18 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  
   private url = `https://api.graph.cool/simple/v1/cjmba2hiq11cv01009591v3v9`;
 
   constructor(private http: HttpClient) { }
 
   public ngOnInit(): void {
+    this.createUser();
     this.allUsers();
   }
 
@@ -26,9 +26,32 @@ export class AppComponent implements OnInit {
           email
         }
       }`
-    }
+    };
+    this.http.post(this.url, body).subscribe(res => console.log(`Query`, res));
+  }
+
+  createUser(): void {
+
+    const body = {
+      query: `
+        mutation CreateNewUser($nome: String!, $email: String!, $password: String!) {
+          createUser(nome: $nome, email: $email, password: $password) {
+            id
+            nome
+            email
+          }
+        }
+      `,
+      variables: {
+        nome: 'Black Panther',
+        email: 'panther@avengers.com',
+        password: '123456'
+      }
+    };
+
     this.http.post(this.url, body)
-    .subscribe(res => console.log(res))
-  };
+      .subscribe(res => console.log('Mutation: ', res));
+
+  }
 
 }
