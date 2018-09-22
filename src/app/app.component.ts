51c21e3a1c723459e1974ea7a6ffd293a2acb304
fabio guelfi <fabioguelfiunix@gmail.com>
2018-09-21 22:35:49 -0300

    query na api graphql com httpclient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-graphql-chat';
+export class AppComponent implements OnInit {
+  
+  private url = `https://api.graph.cool/simple/v1/cjmba2hiq11cv01009591v3v9`;
+
+  constructor(private http: HttpClient) { }
+
+  public ngOnInit(): void {
+    this.allUsers();
+  }
+
+  public allUsers(): void {
+    const body = {
+      query: `
+      query {
+        allUsers {
+          id
+          nome
+          email
+        }
+      }`
+    }
+    this.http.post(this.url, body)
+    .subscribe(res => console.log(res))
+  };
+
 }
